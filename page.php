f60ec1123e7238fa371c4d0f654a36ff9604113e
Fr. Drew <156134904+Frdrew@users.noreply.github.com>
2026-03-24 10:21:31 -0600

    Updated files from fuzzy-octo-system folder

--- page.php
+++ page.php
@@ -1,16 +1,1 @@
-<?php get_header(); ?>
-<section class="page-hero">
-    <div class="container narrow">
-        <h1><?php the_title(); ?></h1>
-    </div>
-</section>
-<section class="section">
-    <div class="container narrow content-card">
-        <?php
-        while (have_posts()) : the_post();
-            the_content();
-        endwhile;
-        ?>
-    </div>
-</section>
-<?php get_footer(); ?>
+<content from fuzzy-octo-system/page.php>
